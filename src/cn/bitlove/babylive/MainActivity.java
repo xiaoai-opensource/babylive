5957040e8d99599e04a8f041427fa6865efe7460
gitSSH <xiaoai_opensource@163.com>
2014-12-11 22:53:53 +0800

    del bin

--- src/cn/bitlove/babylive/MainActivity.java
+++ src/cn/bitlove/babylive/MainActivity.java
@@ -1,14 +1,7 @@
 package cn.bitlove.babylive;
 
-import com.bitlove.babylive.R;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
 
