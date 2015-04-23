d0540ee28697dda0d22826aedef0e3658c5748eb
gitSSH <xiaoai_opensource@163.com>
2015-04-23 23:09:02 +0800

    修改时间轴

---
+++ src/cn/bitlove/babylive/widget/CategoryListAdapter.java
@@ -0,0 +1,91 @@
+package cn.bitlove.babylive.widget;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import cn.bitlove.babylive.R;
+import cn.bitlove.babylive.entity.Record;
+
+/**
+ * 分类列表适配器
+ * @author luoaz
+ */
+public class CategoryListAdapter extends BaseAdapter {
+
+	LayoutInflater mInflater;
+	List<Record> mRecords = new ArrayList<Record>();
+	String mLastCate="";
+	//显示标题的位置
+	HashMap<Integer, Object> mCatePositions=new HashMap<Integer, Object>();
+	
+	public CategoryListAdapter(Context context,List<Record> records) {
+		mRecords = records;
+		mInflater = LayoutInflater.from(context);
+	}
+
+	@Override
+	public int getCount() {
+		return mRecords.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return mRecords.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		Record record = mRecords.get(position);
+		String cate = record.getCreateDate();
+		ViewHolder vh;
+		
+		if(convertView==null){
+			vh = new ViewHolder();
+			convertView = mInflater.inflate(R.layout.category_item, parent, false);
+			vh.tvCate = (TextView) convertView.findViewById(R.id.tvCate);
+			vh.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+			vh.tvContent = (TextView) convertView.findViewById(R.id.tvContent);
+			vh.layoutCate = convertView.findViewById(R.id.layoutCate);
+			if(!mLastCate.equals(cate)){
+				mCatePositions.put(position,true);
+			}
+			
+			convertView.setTag(vh);
+		}else{
+			vh = (ViewHolder) convertView.getTag();
+		}
+		
+		if(mCatePositions.get(position)==null){
+			vh.layoutCate.setVisibility(View.GONE);
+		}else{
+			mLastCate = cate;
+			vh.tvCate.setText(cate);
+			vh.layoutCate.setVisibility(View.VISIBLE);
+		}
+		
+		vh.tvTitle.setText(record.getTitle());
+		vh.tvContent.setText(record.getContent());
+		
+		return convertView;
+	}
+	
+	class ViewHolder{
+		public View layoutCate;
+		public TextView tvCate;
+		public TextView tvTitle;
+		public TextView tvContent;
+	}
+
+}
